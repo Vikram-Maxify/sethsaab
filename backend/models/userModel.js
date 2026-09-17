f1a77f840773f91d6b73aa2efe29cb71197606f5
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-09-17 17:00:31 +0530

    by malik

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,36 +1,42 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    uuid: {
-      type: String,
-      required: true,
-      unique: true,
-      index: true,
-    },
+    {
+        uuid: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+        },
 
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
 
-    mobile: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
+        mobile: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
 
-    password: {
-      type: String,
-      required: true,
-      select: false,
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+
+        wallet: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
     },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const User = mongoose.model("User", userSchema);
