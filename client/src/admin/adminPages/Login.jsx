16cb265bbac7926d33cf59b24c039f04f07c3c64
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-09-21 11:22:17 +0530

    by malik

--- client/src/admin/adminPages/Login.jsx
+++ client/src/admin/adminPages/Login.jsx
@@ -57,7 +57,7 @@ const Login = () => {
     if (adminLogin.fulfilled.match(result)) {
       const from =
         location.state?.from?.pathname ||
-        "/dashboard";
+        "/admin/dashboard";
 
       navigate(from, {
         replace: true,
